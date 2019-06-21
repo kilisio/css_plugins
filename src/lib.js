47eb9ec6fc7f90a4f89380f7cc38656191e6c626
kilisio <pkilisio@gmail.com>
2019-06-21 17:39:54 +0300

    fix(lib.js): re-exported the width function as x from x_func.js

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,7 @@
 "use strict";
 
 export { positioning } from "./components/positioning_plugin.js";
-export { width } from "./components/width_plugin.js";
+export { x } from "./components/x_func.js";
 export { height } from "./components/height_plugin.js";
 export { fontsize } from "./components/font_size_func.js";
 
