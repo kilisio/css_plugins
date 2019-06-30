114f35881f5adbdc08f88889af75aa917988f3c5
kilisio <pkilisio@gmail.com>
2019-06-30 15:21:13 +0300

    fix(lib.js): re-exported scalexy function as scale from scale_func.js

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
 "use strict";
 
 export { pos } from "./components/pos_func.js";
-export { scalexy } from "./components/scalexy_func.js";
+export { scale } from "./components/scale_func.js";
 export { xy } from "./components/xy_func.js";
