82cd14b6dea82a3c89e64678f94b1799b33c03f2
kilisio <pkilisio@gmail.com>
2019-09-24 17:51:52 +0300

    build(rollup.config.js): modified release bunder configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,37 @@
+import clear from 'rollup-plugin-clear';
 import cleanup from 'rollup-plugin-cleanup';
+import copy from 'rollup-plugin-copy';
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
 
 export default {
     input: 'src/lib.js',
     output: {
-        file: 'lib/lib.js',
+        file: 'rls/lib.js',
         format: 'esm'
     },
     plugins: [
-        cleanup()
+        clear({
+            targets: ['rls'],
+        }),
+        resolve(),
+        commonJS({
+              include: 'node_modules/**'
+        }),
+        cleanup(),
+        copy({
+            targets: [
+                {
+                    src: [
+                        'src/assets',
+                        'src/db',
+                        'src/templates',
+                        'src/log',
+                        'src/api.js'
+                    ], 
+                    dest: 'rls'
+                }
+            ]
+        })
     ]
 };
