9de3bc24b5188b366cbd1519bd86b3d2338edf4b
kilisio <pkilisio@gmail.com>
2019-09-24 17:53:09 +0300

    fix(lib.js): modified export object paths

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
 "use strict";
 
-export { pos } from "./components/pos_func.js";
-export { scale } from "./components/scale_func.js";
-export { xy } from "./components/xy_func.js";
+export { pos } from "./pos_func.js";
+export { scale } from "./scale_func.js";
+export { xy } from "./xy_func.js";
